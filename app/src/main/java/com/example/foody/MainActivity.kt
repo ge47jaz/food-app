82a95c70e8688861dfaa39df080888f04637586e
ge47jaz <ge47jaz@mytum.de>
2023-10-09 23:07:23 +0200

    navigation setup

--- app/src/main/java/com/example/foody/MainActivity.kt
+++ app/src/main/java/com/example/foody/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up navigation
+        navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        // Set up action bar
+        val appBarConfiguration = AppBarConfiguration(
+            // top level destinations
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        // Set up bottom navigation
+        bottomNavigationView.setupWithNavController(navController)
+        // Set up action bar with navigation
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
