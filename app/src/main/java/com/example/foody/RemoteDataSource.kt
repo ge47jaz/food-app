dc356fd5b941a726d49af4a6a0fd178384846db7
ge47jaz <ge47jaz@mytum.de>
2023-10-17 10:19:36 +0200

    injected remote data source with FoodRecipeApi

--- app/src/main/java/com/example/foody/RemoteDataSource.kt
+++ app/src/main/java/com/example/foody/RemoteDataSource.kt
@@ -1,5 +1,16 @@
 package com.example.foody
 
-class RemoteDataSource() {
 
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    // inject FoodRecipesApi from NetworkModule
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
